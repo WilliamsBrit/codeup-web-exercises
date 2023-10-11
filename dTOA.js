07081c880bb55d02f30eacad96398b7e402d7015
Brittany Williams <Williams.Brittany5703@gmail.com>
2023-10-11 16:56:49 -0500

    added dTOA.js file

---
+++ dTOA.js
@@ -0,0 +1,18 @@
+// let a = 1
+// console.log(a)
+// let b = a++
+// console.log(b)
+// let c = ++a
+// console.log(c)
+
+//a =1 b = 1 c = 3
+
+// let d = "hello"
+let e  = false
+
+// d++;
+console.log(e++);
+// console.log(d)
+console.log(e)
+
+// let perplexed;
