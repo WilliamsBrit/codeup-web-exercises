a378d524a1f65c3ce45d993ed00eb924798a0a59
Brittany Williams <Williams.Brittany5703@gmail.com>
2023-10-11 15:36:11 -0500

    added operator.js file

---
+++ operators.js
@@ -0,0 +1,60 @@
+console.log('1' + 2)
+// 12
+
+console.log("" ** 4)
+// 0
+console.log(typeof '1' + 2)
+// number
+
+console.log(6 % 4)
+// 2
+
+'3 + 4 is ' + 3 + 4
+console.log('3 + 4 is ' + ( 3 + 4 ))
+//3 + 4 is 34
+// '3 + 4 is ' + ( 3 + 4 )
+
+console.log(0 < 0)
+// false
+
+console.log('false' == false)
+// false
+
+console.log(true == 'true')
+// false
+
+console.log(5 >= -5)
+// true
+
+console.log(!false || false)
+// true
+
+console.log(true || "42")
+//  true
+console.log(!true && !false)
+// false
+
+console.log(6 % 5)
+// 1
+
+console.log(5 < 4 && 1 === 1)
+false
+
+console.log (typeof 'codeup' === 'string')
+// true
+
+console.log('codeup' === 'codeup' && 'codeup' === 'Codeup')
+// false
+
+console.log(4 >= 0 && 1 !== '1')
+// true
+
+console.log (6 % 3 === 0)
+// true
+
+console.log (5 % 2 !== 0)
+// true
+
+
+
+
