c3d38bedd00544c140711032a046380071c3040f
Brittany Williams <Williams.Brittany5703@gmail.com>
2023-10-12 16:10:18 -0500

    added javascript-with-html.html and javasceipt-with-html.js

---
+++ js/javascript-with-html.js
@@ -0,0 +1,16 @@
+"use strict"
+
+let myAge = 5;
+console.log(myAge)
+
+
+let confirmed = confirm("Are you sure you're in the correct cohort?")
+console.log(confirmed)
+
+// prompt
+let userInput = prompt("Please type in your name.")
+
+// alert
+alert(`Great to meet you ${userInput}`)
+console.log("This is my external js file")
+
