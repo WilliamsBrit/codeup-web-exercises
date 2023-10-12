3c534671c597b8a8ec318f94b937d4df209e1090
Brittany Williams <Williams.Brittany5703@gmail.com>
2023-10-12 09:11:20 -0500

    added warmups dir and warmups.js

--- dTOA.js
+++ dTOA.js
@@ -1,18 +1,32 @@
-// let a = 1
-// console.log(a)
-// let b = a++
-// console.log(b)
-// let c = ++a
-// console.log(c)
+let a = 1
+ console.log(a)
+let b = a++
+ console.log(b)
+let c = ++a
+ console.log(c)
 
 //a =1 b = 1 c = 3
 
-// let d = "hello"
-let e  = false
+let d = "hello";
+ console.log(d++)
+let e = false;
+console.log(e++)
 
-// d++;
-console.log(e++);
-// console.log(d)
-console.log(e)
+//d = NaN e = 0
 
-// let perplexed;
+let perplexed;
+perplexed + 2;
+
+console.log(perplexed)
+
+//undefined
+
+let price = 2.7;
+ price.toFixed(2);
+
+ console.log(price)
+
+ 2.7
+
+
+
---
+++ warm-ups/warmUps.js
@@ -0,0 +1,7 @@
+// Brittany Williams
+//"Daily Warm-ups"
+
+let name = "Brittany Williams"
+let year = 2006
+
+console.log("My name is " + name + " and I graduated high school in " + year )
