92f02ba942b2091d2c61f5136eb0fac97230e139
Brittany Williams <Williams.Brittany5703@gmail.com>
2023-10-16 09:30:49 -0500

    capitalize first letter warm up added to warmUps.js

--- warm-ups/warmUps.js
+++ warm-ups/warmUps.js
@@ -5,3 +5,13 @@ let name = "Brittany Williams"
 let year = "2006"
 
 console.log("My name is " + name + " and I graduated high school in " + year )
+
+// sets the parameter of string. Function slices the string leaving the 0 position
+// and then capitalizes the letter in position 0.
+function capitalizeFirstLetter(string) {
+
+    return string.charAt(0).toUpperCase() + string.slice(1)
+
+}
+
+console.log(capitalizeFirstLetter("brittany"))
