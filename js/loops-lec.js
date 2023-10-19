93a84997e5b30db67bd916b2b466317e36120aea
Brittany Williams <Williams.Brittany5703@gmail.com>
2023-10-19 11:04:18 -0500

    updated loops-lectures to add for, break and continue notes

--- js/loops-lec.js
+++ js/loops-lec.js
@@ -31,25 +31,78 @@
 // } while (num < 0)
 
 //mini game
+//
+// const secretNumber = 7;
+// let guess;
+// let attempt = 0;
+//
+// //do-while loop
+//
+// do {
+//     guess = parseInt(prompt("Guess the secret number (1-10)."));
+//     attempt++
+//     if (guess === secretNumber){
+//         console.log("You have guessed the secret number.");
+//     }else {
+//         if(guess < secretNumber) {
+//             console.log("Try a higher number.")
+//         }else {
+//             console.log("Try a lower number.")
+//         }
+//         console.log("Incorrect Guess!" + attempt + "total attempts");
+//     }
+//
+// }while (guess !== secretNumber)
 
-const secretNumber = 7;
-let guess;
-let attempt = 0;
+//----------------------------- For loops---------------------------------
 
-//do-while loop
+// initialization, condition, operation
+//
+// for(let i = 10; i < 20; i++) {
+//     console.log(i)
+// }
+//
+// for(let num = 0; num < 100; num += 5) {
+//     console.log(num)
+// }
+//
+// let hello = "hello";
+// for(let i = 0; i <= hello.length;i++) {
+//     console.log(hello.charAt(i))
+// }
+//
+// for( let i = 0, j = 9; i < 10; i++, j--) {
+//     console.log("For loop iteration #" + i + ',j=' + j);
+// }
 
-do {
-    guess = parseInt(prompt("Guess the secret number (1-10)."));
-    attempt++
-    if (guess === secretNumber){
-        console.log("You have guessed the secret number.");
-    }else {
-        if(guess < secretNumber) {
-            console.log("Try a higher number.")
-        }else {
-            console.log("Try a lower number.")
-        }
-        console.log("Incorrect Guess!" + attempt + "total attempts");
+//Breaking out of a loop
+for(let i = 0; i < 100; i += 5) {
+    console.log(i)
+    // break;
+    // console.log("Snippet never reached")
+}
+
+for(let i = 0; i < 100; i += 5) {
+    console.log(i)
+    if (i === 50) {
+        break;
+    }
+}
+
+// Continue the Next Iteration of a Loop
+for(let i = 1; i <= 5; i++) {
+    if (i === 3) {
+        continue;
     }
 
-}while (guess !== secretNumber)
+    console.log(i)
+}
+
+for(let i = 1; i < 100; i++){
+    if (i % 2 === 0) {
+        continue;
+    }
+    console.log('Here is a lovely even number' + i);
+}
+//continue is like skipping the iteration continue 3...skips 3.
+
