a75aef889c596c418c55ed620abd603995c4f270
Brittany Williams <Williams.Brittany5703@gmail.com>
2023-11-02 14:39:22 -0500

    added flexbox in  practice exercises 1-3

--- warm-ups/warmUps.js
+++ warm-ups/warmUps.js
@@ -71,3 +71,43 @@ const cars = [
 for (let car of cars) {
     console.log(`Make: ${car.make}  Model : ${car.model}   Color: ${car.color}`)
 }
+
+// Create a function that accepts three arguments (arrOfElements, bgColor, textColor), and returns the array of objects with their corresponding properties changed to the inputted values of bgColor and textColor.
+
+    let elements = [
+    {
+        el: "button",
+        style: {
+            backgroundColor: "white",
+            color: "black"
+        },
+        content: "Submit"
+    },
+    {
+        el: "p",
+        style: {
+            backgroundColor: "white",
+            color: "black"
+        },
+        content: "Our company is better at doing company things than your company."
+    },
+    {
+        el: "h2",
+        style: {
+            backgroundColor: "white",
+            color: "black"
+        },
+        content: "Welcome Back!"
+    }
+];
+
+function colors(arrOfElements, bgColor, textColor) {
+
+    for(let i = 0; i < arrOfElements.length;i++) {
+        arrOfElements[i].style.backgroundColor = bgColor
+        arrOfElements[i].style.color = textColor
+    }
+    return arrOfElements
+}
+
+console.log(colors(elements,"red","purple"))
