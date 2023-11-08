a340e74d45b82420da14744a1a5695b238f0af29
Brittany Williams <Williams.Brittany5703@gmail.com>
2023-11-08 15:09:45 -0600

    updated warm ups

--- warm-ups/warmUps.js
+++ warm-ups/warmUps.js
@@ -111,3 +111,18 @@ function colors(arrOfElements, bgColor, textColor) {
 }
 
 console.log(colors(elements,"red","purple"))
+
+
+    for (let i=1; i <= 100; i++){
+        if (i % 3 === 0 && i % 5 === 0){
+            console.log("FizzBuzz")
+        }else if (i % 5 === 0){
+            console.log("Buzz")
+        } else if (i % 3 === 0){
+            console.log("Fizz")
+        }else {
+            console.log(i)
+        }
+    }
+
+
