a8ef379098a2659954764a3210c5540d0d27646a
Brittany Williams <Williams.Brittany5703@gmail.com>
2023-11-15 10:40:57 -0600

    made changes to exercise

--- attributes-script.js
+++ attributes-script.js
@@ -1,47 +1,47 @@
-//
-// document.addEventListener("DOMContentLoaded", function() {
-//     setTimeout( function () {
-//         let profilePic = document.getElementById("profile-pic");
-//         profilePic.src ="img/photo1.png";
-//     }, 2000);
-//     setTimeout( function () {
-//         let profileName = document.getElementById("profile-name");
-//         profileName.innerHTML = "Brittany Williams"
-//     }, 4000);
-//     setTimeout( function () {
-//         let profileDesc = document.getElementById("profile-desc");
-//         profileDesc.classList.add('updated-profile-desc');
-//     }, 6000);
-//     setInterval(function () {
-//         let profileCard = document.getElementById("profile-card");
-//         profileCard.classList.toggle("alternate-background");
-//     }, 2000);
-// });
 
-document.addEventListener("DOMContentLoaded", function () {
-    function updateProfile() {
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout( function () {
         let profilePic = document.getElementById("profile-pic");
+        profilePic.src ="img/photo1.png";
+    }, 2000);
+    setTimeout( function () {
         let profileName = document.getElementById("profile-name");
+        profileName.innerHTML = "Brittany Williams"
+    }, 4000);
+    setTimeout( function () {
         let profileDesc = document.getElementById("profile-desc");
+        profileDesc.classList.add('updated-profile-desc');
+    }, 6000);
+    setInterval(function () {
         let profileCard = document.getElementById("profile-card");
-
-        setTimeout(function () {
-            profilePic.src = "img/photo1.png";
-        }, 2000);
-
-        setTimeout(function () {
-            profileName.innerHTML = "Brittany Williams";
-        }, 4000);
-
-        setTimeout(function () {
-            profileDesc.classList.add('updated-profile-desc');
-        }, 6000);
-
-        setInterval(function () {
-            profileCard.classList.toggle("alternate-background");
-        }, 2000);
-    }
-
-    // Call the function to execute the code
-    updateProfile();
+        profileCard.classList.toggle("alternate-background");
+    }, 2000);
 });
+
+// document.addEventListener("DOMContentLoaded", function () {
+//     function updateProfile() {
+//         let profilePic = document.getElementById("profile-pic");
+//         let profileName = document.getElementById("profile-name");
+//         let profileDesc = document.getElementById("profile-desc");
+//         let profileCard = document.getElementById("profile-card");
+//
+//         setTimeout(function () {
+//             profilePic.src = "img/photo1.png";
+//         }, 2000);
+//
+//         setTimeout(function () {
+//             profileName.innerHTML = "Brittany Williams";
+//         }, 4000);
+//
+//         setTimeout(function () {
+//             profileDesc.classList.add('updated-profile-desc');
+//         }, 6000);
+//
+//         setInterval(function () {
+//             profileCard.classList.toggle("alternate-background");
+//         }, 2000);
+//     }
+//
+//     // Call the function to execute the code
+//     updateProfile();
+// });
