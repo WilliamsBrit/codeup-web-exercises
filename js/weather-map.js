b6fd2ce510c26b63d41c837279619334542d9cee
Brittany Williams <Williams.Brittany5703@gmail.com>
2023-12-04 16:36:50 -0600

    updated weather map project

--- js/weather-map.js
+++ js/weather-map.js
@@ -18,44 +18,49 @@ function getWeatherData() {
             map.setCenter([longitude, latitude]);
 
 
-            let weatherCard = document.querySelector("#weatherCards")
-            let html = ""
 
 
-            fetch(`http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${WEATHER_API_KEY}`)
-                .then(data => data.json())
-                .then(result => {
-                    console.log(result)
-                    const day = result.list;
 
-
-                    for (let i = 0; i < day.length; i += 8) {
-                        const weather = day[i]
-
-                        const date = new Date(weather.dt * 1000);
-
-
-                        const weatherInfo = weather.main.temp;
-                        const tempLike = weather.main.feels_like;
-                        const description = weather.weather[0].description;
-                        const humidity = weather.main.humidity;
-                        // language=html
-                        html += `
-                            <div class="card" style="width: 18rem;">
-                                <p>${date.toLocaleDateString()}</p>
-                                <div> Temp: ${weatherInfo}</div>
-                                <p>Feels Like: ${tempLike}</p>
-                                <p>${description}</p>
-                            </div>`
-
-                    }
-                    weatherCard.innerHTML = html;
-
-                })
+            weatherDataCards(latitude,longitude)
         })
 
-}
 
+
+}
+function weatherDataCards(latitude,longitude) {
+    let weatherCard = document.querySelector("#weatherCards")
+    let html = ""
+    fetch("https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=" + WEATHER_API_KEY + "&units=imperial")
+        .then(data => data.json())
+        .then(result => {
+            console.log(result)
+            const day = result.list;
+
+
+            for (let i = 0; i < day.length; i += 8) {
+                const weather = day[i]
+
+                const date = new Date(weather.dt * 1000);
+
+
+                const weatherInfo = weather.main.temp;
+                const tempLike = weather.main.feels_like;
+                const description = weather.weather[0].description;
+                const humidity = weather.main.humidity;
+                // language=html
+                html += `
+                    <div class="card" style="width: 18rem;">
+                        <p>${date.toLocaleDateString()}</p>
+                        <div> Temp: ${weatherInfo}</div>
+                        <p>Feels Like: ${tempLike}</p>
+                        <p>${description}</p>
+                    </div>`
+
+            }
+            weatherCard.innerHTML = html;
+
+        })
+}
 document.getElementById('weather-form').addEventListener('submit', function (event) {
     event.preventDefault();
     getWeatherData();
